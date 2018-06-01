5a9c62ed8934966e863f88bb62b15971a25dda2c
chenqiaoYi <3260865038@qq.com>
2018-06-01 17:51:41 +0800

    chenqiao 06/01

---
+++ app/webapp/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2018/6/1 0001.
+ */
+$(function(){
+    "use strict";
+    window.onscroll = function(){
+        //获取距离页面顶部的距离
+        //var t = document.documentElement.scrollTop||document.body.scrollTop;
+        //if(t>=100){
+        //    $("#search-wrapper").css({display:"none"});
+        //    $("#search-wrapper-top").css({display:"block"});
+        //}else{
+        //    $("#search-wrapper").css({display:"block"});
+        //    $("#search-wrapper-top").css({display:"none"});
+        //}
+    }
+})

6d79789022697fb6543b395d9512ee49e035c202
jun266a <jun266a@foxmail.com>
2018-06-02 16:11:30 +0800

    更改了部分工程结构和新建了服务和控制模板

---
+++ app/webapp/scripts/control/control.js
@@ -0,0 +1,7 @@
+(function(angular){
+	//这是个control模板
+	var app = angular.module('module.control.placeHolder',[]);//placeHolder--占位符，填页面的名称
+	app.service('placeHolder',['$scope',function($scope){//placeHolder--填文件名(control页面的名称)
+		//TODO
+	}]);
+})(angular);
---
+++ app/webapp/scripts/service/service.js
@@ -0,0 +1,7 @@
+(function(angular){
+	//这是个service模板
+	var app = angular.module('module.service.placeHolder',[]);//placeHolder--占位符，填数据的名称
+	app.service('placeHolder',['$http',function($http){//placeHolder--填文件名(service数据的名称)
+		//TODO
+	}]);
+})(angular);

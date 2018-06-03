715faf58a3f4918daf1ed93c615504d72015333d
chenqiaoYi <3260865038@qq.com>
2018-06-03 16:07:49 +0800

    chenqiao 06/03

--- app/webapp/js/index.js
+++ app/webapp/js/index.js
@@ -1,17 +1,25 @@
 /**
  * Created by Administrator on 2018/6/1 0001.
  */
-$(function(){
-    "use strict";
-    window.onscroll = function(){
-        //获取距离页面顶部的距离
-        //var t = document.documentElement.scrollTop||document.body.scrollTop;
-        //if(t>=100){
-        //    $("#search-wrapper").css({display:"none"});
-        //    $("#search-wrapper-top").css({display:"block"});
-        //}else{
-        //    $("#search-wrapper").css({display:"block"});
-        //    $("#search-wrapper-top").css({display:"none"});
-        //}
-    }
-})
+//$(function(){
+//    window.onscroll = function(){
+//        //获取距离页面顶部的距离
+//        var t = document.documentElement.scrollTop||document.body.scrollTop;
+//        if(t>=100){
+//            $("#search-wrapper").css({display:"none"});
+//            $("#search-wrapper-top").css({display:"block"});
+//            //去顶部的显示出来
+//            $(".ToTop_image").css({display:"block"})
+//            //去顶部的点击事件
+//            $(".ToTop_image").on("click",function(){
+//                console.log("点击了");
+//                document.body.scrollTop = document.documentElement.scrollTop = 0;
+//            })
+//        }else{
+//            $("#search-wrapper").css({display:"block"});
+//            $("#search-wrapper-top").css({display:"none"});
+//            //去顶部的隐藏出来
+//            $(".ToTop_image").css({display:"none"})
+//        }
+//    }
+//})
